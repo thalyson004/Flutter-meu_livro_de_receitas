69c7e9cd255950d0e195709c795f46353c4f7759
Thalyson <thalyson.uece@gmail.com>
2020-07-17 19:04:51 -0300

    Atualização das rotas

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
+      initialRoute: '/recipe',
       routes: {
         '/': (context) => Home(),
         '/recipe': (context) => RecipeScreen(),
